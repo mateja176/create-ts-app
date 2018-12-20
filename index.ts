b95df0469f3254224da767318372d35014f773ff
mytee306 <mateja176@gmail.com>
2018-12-20 23:43:36 +0100

    Adds graphql starter

--- index.ts
+++ index.ts
@@ -2,13 +2,15 @@ import * as commander from "commander";
 import * as fs from "fs-extra";
 import chalk from "chalk";
 import { spawnSync } from "child_process";
+import { toPairs } from "ramda";
 
 commander
   .option("-n, --new [projectName]", "Project name e.g. [my-app]", "my-app")
   .option("-w, --web", "Create parcel based project")
+  .option("-g, --graphql", "Create graphql-yoga based project")
   .parse(process.argv);
 
-const { new: name, web } = commander;
+const { new: name, ...options } = commander;
 
 const templateDirectory = __dirname;
 
@@ -16,8 +18,15 @@ const currentDirectory = `${process.cwd()}/${name}`;
 
 console.log(chalk.blue(`🛠 Building project ${name}`));
 
+const selectProject = (options: { [key: string]: boolean }) => {
+  const [key] = [...toPairs(options), ["node", true]].find(
+    ([_, value]) => !!value
+  );
+  return key;
+};
+
 fs.copySync(
-  `${templateDirectory}/templates/${web ? "web" : "node"}`,
+  `${templateDirectory}/templates/${selectProject(options)}`,
   currentDirectory
 );
 
---
+++ templates/graphql/src/index.ts
@@ -0,0 +1,15 @@
+import { GraphQLServer } from "graphql-yoga";
+import * as fs from "fs";
+
+const resolvers = {
+  Query: {
+    hello: (_: any, { name }: { name: string }) => `Hello ${name || "World"}`
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs: fs.readFileSync("./schema.graphql", { encoding: "UTF-8" }),
+  resolvers
+});
+
+server.start(() => console.log("Server is running on http://localhost:4000"));
