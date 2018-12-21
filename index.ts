1287aae5fe87961142101b04d98bef376b8c871d
mytee306 <mateja176@gmail.com>
2018-12-21 23:29:41 +0100

    Adds no-install option

--- index.ts
+++ index.ts
@@ -8,11 +8,12 @@ commander
   .option("-n, --new [projectName]", "Project name e.g. [my-app]", "my-app")
   .option("-w, --web", "Create parcel based project")
   .option("-g, --graphql", "Create graphql-yoga based project")
+  .option("-i, --install", "Install node modules")
   .parse(process.argv);
 
-const { new: name, web, graphql } = commander;
+const { new: name, web, graphql, install } = commander;
 
-const options = { web, graphql };
+const options = { web, graphql, node: true };
 
 const templateDirectory = __dirname;
 
@@ -21,10 +22,9 @@ const currentDirectory = `${process.cwd()}/${name}`;
 console.log(chalk.blue(`🛠 Building project ${name}`));
 
 const selectProject = (options: { [key: string]: boolean }) => {
-  const [key] = find<Array<string | boolean>>(([_, value]) => !!value)([
-    ...toPairs(options),
-    ["node", true]
-  ]);
+  const [key] = find<Array<string | boolean>>(([_, value]) => value as boolean)(
+    toPairs(options)
+  );
 
   return key;
 };
@@ -48,9 +48,12 @@ spawnSync("git", ["init"], { cwd: currentDirectory });
 
 console.log(chalk.green("📘 Initialized git repository"));
 
-console.log(chalk.blue("⚙ Installing node modules"));
+if (install) {
+  console.log(chalk.blue("⚙ Installing node modules"));
 
-spawnSync("npm", ["install"], { cwd: currentDirectory });
+  spawnSync("npm", ["install"], { cwd: currentDirectory });
+}
 
 console.log(chalk.green("🚀 Project created successfully"));
+
 console.log(chalk.bgBlue.black(`cd ${name} && npm start`));
