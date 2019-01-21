dab61c0fd8f297356c05ac044edd6e7566d4275b
Mateja Petrovic <mateja176@gmail.com>
2019-01-21 22:46:14 +0100

    Adds By to count action types

--- templates/redux/src/redux/actions/types/count.ts
+++ templates/redux/src/redux/actions/types/count.ts
@@ -1,5 +1,5 @@
-const INCREMENT_BY = "[Count] Increment";
+const INCREMENT_BY = "[Count] Increment By";
 
-const DECREMENT_BY = "[Count] Decrement";
+const DECREMENT_BY = "[Count] Decrement By";
 
 export { INCREMENT_BY, DECREMENT_BY };
