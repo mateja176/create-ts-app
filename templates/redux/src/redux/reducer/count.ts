41b6cefc194071dc7e2c60dd4ce96d67adbc4ef1
Mateja Petrovic <mateja176@gmail.com>
2019-01-21 22:49:57 +0100

    Reducer works with count and not an object

--- templates/redux/src/redux/reducer/count.ts
+++ templates/redux/src/redux/reducer/count.ts
@@ -1,17 +1,15 @@
 import { INCREMENT_BY, DECREMENT_BY } from "../actions/types/count";
 import CountAction from "../actions/count";
 
-const initialState = { count: 0 };
+const initialState = 0;
 
 const count = (state = initialState, { type, payload }: CountAction) => {
-  const { count } = state;
-
   switch (type) {
     case INCREMENT_BY:
-      return { count: count + payload };
+      return state + payload;
 
     case DECREMENT_BY:
-      return { count: count - payload };
+      return state - payload;
 
     default:
       return state;
