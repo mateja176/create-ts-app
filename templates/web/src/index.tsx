bbe2c7705d1e322552c0c9a53237cd46fb0cde1f
Mateja Petrovic <mateja176@gmail.com>
2019-01-21 21:58:10 +0100

    Parcel comes with HMR out of the box

--- templates/web/src/index.tsx
+++ templates/web/src/index.tsx
@@ -2,10 +2,6 @@ import * as React from "react";
 import { render } from "react-dom";
 import App from "./App";
 
-declare const module: any;
-
 const renderApp = () => render(<App />, document.querySelector(".app"));
 
 renderApp();
-
-module.hot.accept("./App", renderApp);
