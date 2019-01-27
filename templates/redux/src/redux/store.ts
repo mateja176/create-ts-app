136976ce8c0414f545456166b6e4a9c972965e6a
Mateja Petrovic <mateja176@gmail.com>
2019-01-27 21:56:10 +0100

    Adds redux-devtools-extension

--- templates/redux/src/redux/store.ts
+++ templates/redux/src/redux/store.ts
@@ -1,10 +1,11 @@
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./reducer";
 
 type State = {
   count: number;
 };
 
-export default createStore(reducers);
+export default createStore(reducers, composeWithDevTools());
 
 export { State };
